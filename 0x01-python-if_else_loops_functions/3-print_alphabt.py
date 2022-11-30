902d3cbb59c1c9a0133cdbb1ff3d90a53ae590a2
Ejenihu Christopher Kelechi <ejenihukelechi@gmail.com>
2022-11-30 21:55:28 +0000

    yask

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,10 @@
+
+Skip to content
+Tolulope05 /
+alx-higher_level_programming
+Public
+#!/usr/bin/python3
+# Author - Ejenihu kelechi
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
